d74750392c4fb47dfb982ee9154d5e8f739beacc
khandekar saleh <ksaleh@PC192983.cts.com>
2015-07-03 10:47:30 -0500

    build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/local/java/jdk1.6.0_29
+export CATALINA_HOME=/home/ksaleh/Desktop/work/MyWork/apache-tomcat-6.0.44
+export CATALINA_BASE=/home/ksaleh/Desktop/work/MyWork/apache-tomcat-6.0.44
+gradle "$@"
